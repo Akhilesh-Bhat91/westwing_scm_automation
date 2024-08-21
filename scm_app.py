8c931debf2b0d287ce0c9fb076ae2a9da6178eca
Akhilesh-Bhat91 <akhilesh.bhat-external@westwing.de>
2024-08-21 07:43:46 +0200

    Update scm_app.py

--- scm_app.py
+++ scm_app.py
@@ -1,2 +1,12 @@
 import streamlit as st
-st.write("Hello World!")
+
+def run():
+    category = ['--Select--', '1', '2', '3']
+    cat_op = st.selectbox('Select your Category', category)
+    if cat_op == category[0]:
+        print('Please select something')
+    else:
+        print(cat_op)
+        
+run()
+# st.write("Hello World!")
