6efdd8476af7ed2eda027112f9a435d0b910709e
Akhilesh-Bhat91 <akhilesh.bhat-external@westwing.de>
2024-08-21 21:16:59 +0200

    Update scm_app.py

--- scm_app.py
+++ scm_app.py
@@ -1,16 +1,17 @@
 import streamlit as st
 from PIL import Image
+st.set_page_config(page_title='The cool westwing SCM automation app', page_icon='./images/westwing_logo.jpeg')
 def run():
-    st.title("Westwing SCM App: For all the cool automation stuff")
-    image = Image.open('./images/westwing_logo.jpeg')
+    # st.title("The cool westwing SCM automation app")
+    # image = Image.open('./images/westwing_logo.jpeg')
 
     col1, col2, col3 = st.columns([3, 5, 3])
-    with col1:
-        st.write("")
-    with col2:
-        st.image(image, use_column_width=False)
-    with col3:
-        st.write("")
+    # with col1:
+    #     st.write("")
+    # with col2:
+    #     st.image(image, use_column_width=False)
+    # with col3:
+    #     st.write("")
         
     if st.button("PRL PO Placement", type="primary"):
         st.write("Trying to automate PRL PO placement")
