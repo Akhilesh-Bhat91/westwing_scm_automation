5322d6d03446727df432f0bd69abc99fcc62b374
Akhilesh-Bhat91 <akhilesh.bhat-external@westwing.de>
2024-08-22 12:37:30 +0200

    Update scm_app.py

--- scm_app.py
+++ scm_app.py
@@ -2,6 +2,14 @@ import streamlit as st
 import pandas as pd
 # from PIL import Image
 st.set_page_config(page_title='The cool Westwing SCM automation app', page_icon='./images/westwing_logo.jpeg')
+
+def file_upload():
+    prl_input_file = st.session_state["new_file"]
+    dataframe = pd.read_excel(prl_input_file)
+    st.write("filename:", prl_input_file.name)
+    st.write(dataframe)
+    # dataframe = pd.read_csv(prl_input_file)
+    # st.write(dataframe)
 def run():
     st.title("The cool Westwing SCM automation app")
     # image = Image.open('./images/westwing_logo.jpeg')
@@ -16,14 +24,8 @@ def run():
         
     if st.button("PRL PO Placement", type="primary"):
         st.write("Trying to automate PRL PO placement")
-        prl_input_file = st.file_uploader("Choose the PRL Input file", type=['xlsx'])
-        st.write("hello1")
-        if prl_input_file is not None:
-            st.write("hello")
-            dataframe = pd.read_excel(prl_input_file)
-            st.write("filename:", prl_input_file.name)
-            st.write(dataframe)
-            # dataframe = pd.read_csv(prl_input_file)
-            # st.write(dataframe)
+        st.session_state.uploaded_file = st.file_uploader("Choose the PRL Input file", accept_multiple_files=False, key="prl_input_file", on_change=file_upload)
+        # prl_input_file = st.file_uploader("Choose the PRL Input file", type=['xlsx'])
+        
 run()
 
