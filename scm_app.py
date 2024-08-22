6248e41ff822312b1799112b29ce124c8f488e0c
Akhilesh-Bhat91 <akhilesh.bhat-external@westwing.de>
2024-08-22 08:54:47 +0200

    Update scm_app.py

--- scm_app.py
+++ scm_app.py
@@ -5,16 +5,20 @@ def run():
     st.title("The cool Westwing SCM automation app")
     # image = Image.open('./images/westwing_logo.jpeg')
 
-    col1, col2, col3 = st.columns([3, 5, 3])
-    with col1:
-        st.write("")
-    with col2:
-        st.image(image, use_column_width=False)
-    with col3:
-        st.write("")
+    # col1, col2, col3 = st.columns([3, 5, 3])
+    # with col1:
+    #     st.write("")
+    # with col2:
+    #     st.image(image, use_column_width=False)
+    # with col3:
+    #     st.write("")
         
     if st.button("PRL PO Placement", type="primary"):
         st.write("Trying to automate PRL PO placement")
-        
+        prl_input_file = st.file_uploader("Choose the PRL Input file", type=[xlsx])
+        # dataframe = pd.read_csv(uploaded_file)
+        # st.write(dataframe)
+        dataframe = pd.read_excel(uploaded_file)
+        st.write(dataframe)
 run()
 
