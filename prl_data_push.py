986ec2096e4a41c5793769a2cd3c83bc48e1bff9
Akhilesh-Bhat91 <akhilesh.bhat-external@westwing.de>
2024-08-22 15:45:59 +0200

    Update prl_data_push.py

--- prl_data_push.py
+++ prl_data_push.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service]
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
