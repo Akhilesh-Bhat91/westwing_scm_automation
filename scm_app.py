c10e43563b81b6d2be4211c64c8e871aed2db089
Akhilesh-Bhat91 <akhilesh.bhat-external@westwing.de>
2024-08-22 12:12:11 +0200

    Update scm_app.py

--- scm_app.py
+++ scm_app.py
@@ -19,6 +19,7 @@ def run():
         prl_input_file = st.file_uploader("Choose the PRL Input file", type=['xlsx'])
         
         if prl_input_file is not None:
+            st.write("hello")
             dataframe = pd.read_excel(prl_input_file)
             st.write("filename:", prl_input_file.name)
             st.write(dataframe)
